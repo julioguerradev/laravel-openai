bc46e288aedaaf890dc1014c334f4c458d73a29b
julio.guerra <julio.guerra@tridia.com.br>
2024-02-19 21:26:16 -0300

    :bug: fix: running migrate

--- database/migrations/2024_02_20_000123_create_sales_comission_view.php
+++ database/migrations/2024_02_20_000123_create_sales_comission_view.php
@@ -9,14 +9,16 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         $query = DB::table('sales as s')
-            ->join('sellers as sl', 'sl.id', '=', 's.seller_id')
-            ->join('clients as cl', 'cl.id', '=', 's.client_id')
-            ->join('companies as cp', 'cp.id', '=', 'sl.company_id')
-            ->join('addresses as d', 'ad.id', '=', 'cl.address_id')
+            ->join('sellers as sl', 'sl.id', '=' ,'s.seller_id')
+            ->join('clients as cl', 'cl.id','=','s.client_id')
+            ->join('companies as cp', 'cp.id','=','sl.company_id')
+            ->join('addresses as ad', 'ad.id', '=', 'cl.address_id')
             ->join('users as us', 'us.id', '=', 'sl.user_id')
             ->join('users as uc', 'uc.id', '=', 'cl.user_id')
             ->selectRaw("
@@ -28,7 +30,7 @@ return new class extends Migration
                 s.sold_at,
                 s.status,
                 s.total_amount,
-                round(s.total_amount * cp.commission_rate / 100) as commission
+                round(s.total_amount * cp.commision_rate / 100) as commission
             ")->toSql();
 
         // MATERIALIZED - ARMAZENA O RESULTADO DA CONSULTA NO CASH
@@ -37,9 +39,11 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('sales_comission_view');
+        DB::statement("DROP MATERIALIZED VIEW sales_commission_view");
     }
 };
