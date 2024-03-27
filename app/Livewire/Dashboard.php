667793c33c8113ae4cbed899c787ee9658d600c3
julio.guerra <julio.guerra@tridia.com.br>
2024-03-27 13:00:56 -0300

    :sparkles: feat: FInalizando integração

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -28,13 +28,14 @@ class Dashboard extends Component
         $fields = implode(',',SalesCommission::getColumns());
 
         $this->config =  OpenAI::completions()->create([
-            'model' => 'text-davinci-003',
+            'model' => 'gpt-3.5-turbo-instruct',
             'prompt' => "Considerando a lista de campos ($fields), gere uma configuração json do Vega-lite v5 (sem campo de dados e com descrição) que atenda o seguinte pedido {$this->question}. Resposta:",
             'max_tokens' => 1500
         ])->choices[0]->text;
 
         $this->config = str_replace("\n", "", $this->config);
         $this->config = json_decode($this->config, true);
+
 
         $this->dataset = ["values" => SalesCommission::inRandomOrder()->limit(100)->get()->toArray()];
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,14 @@
 import './bootstrap';
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import dashboard from "./components/dashboard";
 
-import Alpine from 'alpinejs';
+// import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 Alpine.data('dashboard', dashboard);
-Alpine.start();
+
+Livewire.start();
+
+import.meta.glob([
+  '../assets/**',
+]);
